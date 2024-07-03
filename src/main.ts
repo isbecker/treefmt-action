7ebc60c3b9ad841109f96c9933ad9e305e941094
Ian Becker <ian@beckr.dev>
2024-07-02 21:57:44 -0400

    feat: adds functional action

--- src/main.ts
+++ src/main.ts
@@ -1,117 +1,194 @@
 import * as core from "@actions/core";
 import * as exec from "@actions/exec";
+import * as tc from "@actions/tool-cache";
 import { Octokit } from "@octokit/rest";
 import * as fs from "fs";
 import * as path from "path";
-import * as tc from "@actions/tool-cache";
 
 async function run() {
 	try {
-		// Get the inputs
 		const inputVersion = core.getInput("version") || "latest";
-		const configFile = core.getInput("config-file") || "treefmt.toml";
-
-		// Determine the platform and architecture
-		const platform = process.platform;
-		let arch = process.arch;
+		const configFile = core.getInput("config_file") || "treefmt.toml";
 		const token = core.getInput("github_token");
+		const allowMissingFormatter =
+			core.getInput("allow_missing_formatter") === "true";
+		const workingDirectory = core.getInput("working_directory") || ".";
+		const noCache = core.getInput("no_cache") === "true";
+		const failOnChange = core.getInput("fail_on_change") === "true";
+		const formatters = core.getInput("formatters");
+		const treeRoot = core.getInput("tree_root");
+		const treeRootFile = core.getInput("tree_root_file");
+		const walk = core.getInput("walk") || "auto";
+		const verbose = core.getInput("verbose") === "true";
+		const onUnmatched = core.getInput("on_unmatched") || "warn";
+
+		const platform = process.platform;
+		const arch = process.arch;
 		const octokit = new Octokit({ auth: token });
 
 		let version = inputVersion;
-		let latest = false;
-		let filename = "";
+		let latest = inputVersion === "latest";
+		let filename = getFilename(platform, arch, version);
 
-		if (platform === "win32") {
-			switch (arch) {
-				case "x64":
-					filename = "treefmt-x86_64-pc-windows-msvc.zip";
-					break;
-				case "arm64":
-					filename = "treefmt-aarch64-pc-windows-msvc.zip";
-					break;
-				default:
-					throw new Error(
-						`Unsupported platform/arch combination: ${platform}-${arch}`,
-					);
-			}
-			version = "v0.6.1";
-			latest = false;
-		} else {
-			switch (`${platform}-${arch}`) {
-				case "linux-x64":
-					filename = `treefmt_${inputVersion}_linux_amd64.tar.gz`;
-					break;
-				case "linux-arm64":
-					filename = `treefmt_${inputVersion}_linux_${arch}.tar.gz`;
-					break;
-				case "darwin-x64":
-					filename = `treefmt_${inputVersion}_darwin_amd64.tar.gz`;
-					break;
-				case "darwin-arm64":
-					filename = `treefmt_${inputVersion}_darwin_${arch}.tar.gz`;
-					break;
-				default:
-					throw new Error(
-						`Unsupported platform/arch combination: ${platform}-${arch}`,
-					);
-			}
-			latest = inputVersion === "latest";
-			version = inputVersion;
-		}
-
-		// Fetch the release
-		let release;
 		if (latest) {
-			release = await octokit.rest.repos.getLatestRelease({
-				owner: "numtide",
-				repo: "treefmt",
-			});
-			version = release.data.tag_name;
+			const release = await getLatestRelease(octokit);
+			version = release.data.tag_name.replace("v", "");
 			filename = filename.replace("latest", version);
+		}
+
+		const toolPath = tc.find("treefmt", version, arch);
+		if (toolPath) {
+			core.addPath(toolPath);
 		} else {
-			release = await octokit.rest.repos.getReleaseByTag({
-				owner: "numtide",
-				repo: "treefmt",
-				tag: version,
-			});
+			const downloadUrl = await getDownloadUrl(
+				octokit,
+				version,
+				filename,
+				latest,
+			);
+			const cachedPath = await downloadAndCacheTreefmt(
+				downloadUrl,
+				platform,
+				version,
+				arch,
+			);
+			core.addPath(cachedPath);
+			ensureExecutable(platform, cachedPath);
 		}
 
-		const asset = release.data.assets.find((a) => a.name === filename);
-		if (!asset) {
-			throw new Error(`Asset not found: ${filename}`);
+		const treefmtArgs = constructArgs({
+			configFile,
+			allowMissingFormatter,
+			workingDirectory,
+			noCache,
+			failOnChange,
+			formatters,
+			treeRoot,
+			treeRootFile,
+			walk,
+			verbose,
+			onUnmatched,
+		});
+
+		const exitCode = await exec.exec("treefmt", treefmtArgs);
+		if (exitCode !== 0) {
+			throw new Error(`treefmt failed with exit code ${exitCode}`);
 		}
-
-		const downloadUrl = asset.browser_download_url;
-
-		// Download and extract treefmt
-		const downloadPath = await tc.downloadTool(downloadUrl);
-		let extractedPath;
-		if (platform === "win32") {
-			extractedPath = await tc.extractZip(downloadPath);
-		} else {
-			extractedPath = await tc.extractTar(downloadPath);
-		}
-
-		// Add the binary to PATH
-		const cachedPath = await tc.cacheDir(
-			extractedPath,
-			"treefmt",
-			version,
-			arch,
-		);
-		core.addPath(cachedPath);
-
-		// Make sure the binary is executable on Unix-like systems
-		if (platform !== "win32") {
-			const treefmtPath = path.join(cachedPath, "treefmt");
-			fs.chmodSync(treefmtPath, "755");
-		}
-
-		// Run treefmt
-		await exec.exec(`treefmt --config-file=${configFile}`);
 	} catch (error: any) {
-		core.setFailed(error.message);
+		core.setFailed(`Action failed with error: ${error.message}`);
 	}
+}
+
+function getFilename(platform: string, arch: string, version: string): string {
+	if (platform === "win32") {
+		if (arch === "x64") {
+			return "treefmt-x86_64-pc-windows-msvc.zip";
+		} else if (arch === "arm64") {
+			return "treefmt-aarch64-pc-windows-msvc.zip";
+		} else {
+			throw new Error(
+				`Unsupported platform/arch combination: ${platform}-${arch}`,
+			);
+		}
+	} else {
+		switch (`${platform}-${arch}`) {
+			case "linux-x64":
+				return `treefmt_${version}_linux_amd64.tar.gz`;
+			case "linux-arm64":
+				return `treefmt_${version}_linux_${arch}.tar.gz`;
+			case "darwin-x64":
+				return `treefmt_${version}_darwin_amd64.tar.gz`;
+			case "darwin-arm64":
+				return `treefmt_${version}_darwin_${arch}.tar.gz`;
+			default:
+				throw new Error(
+					`Unsupported platform/arch combination: ${platform}-${arch}`,
+				);
+		}
+	}
+}
+
+async function getLatestRelease(octokit: Octokit) {
+	return await octokit.rest.repos.getLatestRelease({
+		owner: "numtide",
+		repo: "treefmt",
+	});
+}
+
+async function getDownloadUrl(
+	octokit: Octokit,
+	version: string,
+	filename: string,
+	latest: boolean,
+) {
+	let release;
+	if (latest) {
+		release = await getLatestRelease(octokit);
+		version = release.data.tag_name.replace("v", "");
+		filename = filename.replace("latest", version);
+	} else {
+		release = await octokit.rest.repos.getReleaseByTag({
+			owner: "numtide",
+			repo: "treefmt",
+			tag: version,
+		});
+	}
+
+	const asset = release.data.assets.find((a) => a.name === filename);
+	if (!asset) {
+		throw new Error(`Asset not found: ${filename}`);
+	}
+
+	return asset.browser_download_url;
+}
+
+async function downloadAndCacheTreefmt(
+	downloadUrl: string,
+	platform: string,
+	version: string,
+	arch: string,
+) {
+	const downloadPath = await tc.downloadTool(downloadUrl);
+	const extractedPath =
+		platform === "win32"
+			? await tc.extractZip(downloadPath)
+			: await tc.extractTar(downloadPath);
+	return await tc.cacheDir(extractedPath, "treefmt", version, arch);
+}
+
+function ensureExecutable(platform: string, cachedPath: string) {
+	if (platform !== "win32") {
+		const treefmtPath = path.join(cachedPath, "treefmt");
+		fs.chmodSync(treefmtPath, "755");
+	}
+}
+
+function constructArgs(options: {
+	configFile: string;
+	allowMissingFormatter: boolean;
+	workingDirectory: string;
+	noCache: boolean;
+	failOnChange: boolean;
+	formatters?: string;
+	treeRoot?: string;
+	treeRootFile?: string;
+	walk: string;
+	verbose: boolean;
+	onUnmatched: string;
+}): string[] {
+	const args = ["--config-file", options.configFile];
+	if (options.allowMissingFormatter) args.push("--allow-missing-formatter");
+	if (options.workingDirectory)
+		args.push("--working-directory", options.workingDirectory);
+	if (options.noCache) args.push("--no-cache");
+	if (options.failOnChange) args.push("--fail-on-change");
+	if (options.formatters) args.push("--formatters", options.formatters);
+	if (options.treeRoot) args.push("--tree-root", options.treeRoot);
+	if (options.treeRootFile) args.push("--tree-root-file", options.treeRootFile);
+	if (options.walk) args.push("--walk", options.walk);
+	if (options.verbose) args.push("--vv");
+	if (options.onUnmatched) args.push("--on-unmatched", options.onUnmatched);
+	return args;
 }
 
 run();
