83f1a8848846721fd74a22636b9e7a6e2fc7db46
Ian Becker <ian@beckr.dev>
2024-12-31 20:20:57 -0500

    feat: code cleanup and general improvements

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,84 @@
 import * as core from "@actions/core";
 import * as exec from "@actions/exec";
+import * as github from "@actions/github";
 import * as tc from "@actions/tool-cache";
-import { Octokit } from "@octokit/rest";
 import * as fs from "fs";
 import * as path from "path";
 
+class ActionInputs {
+	version: string;
+	configFile: string;
+	githubToken: string;
+	allowMissingFormatter: boolean;
+	workingDir: string;
+	noCache: boolean;
+	failOnChange: boolean;
+	formatters: string;
+	treeRoot: string;
+	treeRootFile: string;
+	walk: string;
+	verbose: string;
+	onUnmatched: string;
+	ci: boolean;
+	clearCache: boolean;
+	excludes: string;
+	stdin: boolean;
+	init: boolean;
+
+	constructor() {
+		this.version = this.inputOrDefault("version", "latest") as string;
+		this.configFile = this.inputOrDefault(
+			"config_file",
+			"treefmt.toml",
+		) as string;
+		this.githubToken = this.inputOrDefault("github_token") as string;
+		this.allowMissingFormatter = this.inputOrDefault(
+			"allow_missing_formatter",
+		) as boolean;
+		this.workingDir = this.inputOrDefault("working_dir") as string;
+		this.noCache = this.inputOrDefault("no_cache") as boolean;
+		this.failOnChange = this.inputOrDefault("fail_on_change") as boolean;
+		this.formatters = this.inputOrDefault("formatters") as string;
+		this.treeRoot = this.inputOrDefault("tree_root") as string;
+		this.treeRootFile = this.inputOrDefault("tree_root_file") as string;
+		this.walk = this.inputOrDefault("walk", "auto") as string;
+		this.verbose = this.inputOrDefault("verbose", "0") as string;
+		this.onUnmatched = this.inputOrDefault("on_unmatched", "warn") as string;
+		this.ci = this.inputOrDefault("ci") as boolean;
+		this.clearCache = this.inputOrDefault("clear_cache") as boolean;
+		this.excludes = this.inputOrDefault("excludes") as string;
+		this.stdin = this.inputOrDefault("stdin") as boolean;
+		this.init = this.inputOrDefault("init") as boolean;
+	}
+
+	private inputOrDefault<T extends string | boolean>(
+		name: string,
+		defaultValue?: T,
+	): T {
+		const value = core.getInput(name);
+		if (value === "") {
+			return defaultValue!;
+		}
+		if (value === "true") {
+			return true as T;
+		} else if (value === "false") {
+			return false as T;
+		}
+		return value as T;
+	}
+}
+
 async function run() {
 	try {
-		const inputVersion = core.getInput("version") || "latest";
-		const configFile = core.getInput("config_file") || "treefmt.toml";
-		const token = core.getInput("github_token");
-		const allowMissingFormatter =
-			core.getInput("allow_missing_formatter") === "true";
-		const workingDir = core.getInput("working_dir");
-		const noCache = core.getInput("no_cache") === "true";
-		const failOnChange = core.getInput("fail_on_change") === "true";
-		const formatters = core.getInput("formatters");
-		const treeRoot = core.getInput("tree_root");
-		const treeRootFile = core.getInput("tree_root_file");
-		const walk = core.getInput("walk") || "auto";
-		const verbose = core.getInput("verbose") || "0";
-		const onUnmatched = core.getInput("on_unmatched") || "warn";
-		const ci = core.getInput("ci") === "true";
-		const clearCache = core.getInput("clear_cache") === "true";
-		const excludes = core.getInput("excludes");
-		const stdin = core.getInput("stdin") === "true";
-		const init = core.getInput("init") === "true";
-
+		const inputs = new ActionInputs();
 		const platform = process.platform;
 		const arch = process.arch;
-		const octokit = new Octokit({ auth: token });
+		const octokit: ReturnType<typeof github.getOctokit> = github.getOctokit(
+			inputs.githubToken,
+		);
 
-		let version = inputVersion;
-		let latest = inputVersion === "latest";
+		let version = inputs.version;
+		let latest = inputs.version === "latest";
 		let filename = getFilename(platform, arch, version);
 
 		if (latest) {
@@ -56,29 +102,11 @@ async function run() {
 				arch,
 			);
 			ensureExecutable(platform, cachedPath);
-			core.addPath(cachedPath);
 			toolPath = cachedPath;
 		}
 		core.addPath(toolPath);
 
-		const treefmtArgs = constructArgs({
-			configFile,
-			allowMissingFormatter,
-			workingDir,
-			noCache,
-			failOnChange,
-			formatters,
-			treeRoot,
-			treeRootFile,
-			walk,
-			verbose,
-			onUnmatched,
-			ci,
-			clearCache,
-			excludes,
-			stdin,
-			init,
-		});
+		const treefmtArgs = constructArgs(inputs);
 
 		await exec.exec("treefmt", treefmtArgs);
 	} catch (error: any) {
@@ -115,7 +143,9 @@ function getFilename(platform: string, arch: string, version: string): string {
 	}
 }
 
-async function getLatestRelease(octokit: Octokit) {
+async function getLatestRelease(
+	octokit: ReturnType<typeof github.getOctokit>,
+): Promise<ReturnType<typeof octokit.rest.repos.getLatestRelease>> {
 	return await octokit.rest.repos.getLatestRelease({
 		owner: "numtide",
 		repo: "treefmt",
@@ -123,12 +153,15 @@ async function getLatestRelease(octokit: Octokit) {
 }
 
 async function getDownloadUrl(
-	octokit: Octokit,
+	octokit: ReturnType<typeof github.getOctokit>,
 	version: string,
 	filename: string,
 	latest: boolean,
-) {
-	let release;
+): Promise<string> {
+	let release: Awaited<
+		| ReturnType<typeof octokit.rest.repos.getLatestRelease>
+		| ReturnType<typeof octokit.rest.repos.getReleaseByTag>
+	>;
 	if (latest) {
 		release = await getLatestRelease(octokit);
 		version = release.data.tag_name.replace("v", "");
@@ -154,7 +187,7 @@ async function downloadAndCacheTreefmt(
 	platform: string,
 	version: string,
 	arch: string,
-) {
+): Promise<string> {
 	const downloadPath = await tc.downloadTool(downloadUrl);
 	const extractedPath =
 		platform === "win32"
@@ -163,49 +196,33 @@ async function downloadAndCacheTreefmt(
 	return await tc.cacheDir(extractedPath, "treefmt", version, arch);
 }
 
-function ensureExecutable(platform: string, cachedPath: string) {
+function ensureExecutable(platform: string, cachedPath: string): void {
 	if (platform !== "win32") {
 		const treefmtPath = path.join(cachedPath, "treefmt");
 		fs.chmodSync(treefmtPath, "755");
 	}
 }
 
-function constructArgs(options: {
-	configFile: string;
-	allowMissingFormatter: boolean;
-	workingDir: string;
-	noCache: boolean;
-	failOnChange: boolean;
-	formatters?: string;
-	treeRoot?: string;
-	treeRootFile?: string;
-	walk: string;
-	verbose: string;
-	onUnmatched: string;
-	ci: boolean;
-	clearCache: boolean;
-	excludes?: string;
-	stdin: boolean;
-	init: boolean;
-}): string[] {
-	const args = ["--config-file", options.configFile];
-	if (options.allowMissingFormatter) args.push("--allow-missing-formatter");
-	if (options.workingDir) args.push("-C", options.workingDir);
-	if (options.noCache) args.push("--no-cache");
-	if (options.failOnChange) args.push("--fail-on-change");
-	if (options.formatters) args.push("--formatters", options.formatters);
-	if (options.treeRoot) args.push("--tree-root", options.treeRoot);
-	if (options.treeRootFile) args.push("--tree-root-file", options.treeRootFile);
-	if (options.walk) args.push("--walk", options.walk);
-	if (options.verbose && Number(options.verbose) > 0) {
-		args.push("-" + "v".repeat(Number(options.verbose)));
+function constructArgs(inputs: ActionInputs): string[] {
+	const args = [];
+	if (inputs.configFile) args.push("--config-file", inputs.configFile);
+	if (inputs.allowMissingFormatter) args.push("--allow-missing-formatter");
+	if (inputs.workingDir) args.push("-C", inputs.workingDir);
+	if (inputs.noCache) args.push("--no-cache");
+	if (inputs.failOnChange) args.push("--fail-on-change");
+	if (inputs.formatters) args.push("--formatters", inputs.formatters);
+	if (inputs.treeRoot) args.push("--tree-root", inputs.treeRoot);
+	if (inputs.treeRootFile) args.push("--tree-root-file", inputs.treeRootFile);
+	if (inputs.walk) args.push("--walk", inputs.walk);
+	if (inputs.verbose && Number(inputs.verbose) > 0) {
+		args.push("-" + "v".repeat(Number(inputs.verbose)));
 	}
-	if (options.onUnmatched) args.push("--on-unmatched", options.onUnmatched);
-	if (options.ci) args.push("--ci");
-	if (options.clearCache) args.push("--clear-cache");
-	if (options.excludes) args.push("--excludes", options.excludes);
-	if (options.stdin) args.push("--stdin");
-	if (options.init) args.push("--init");
+	if (inputs.onUnmatched) args.push("--on-unmatched", inputs.onUnmatched);
+	if (inputs.ci) args.push("--ci");
+	if (inputs.clearCache) args.push("--clear-cache");
+	if (inputs.excludes) args.push("--excludes", inputs.excludes);
+	if (inputs.stdin) args.push("--stdin");
+	if (inputs.init) args.push("--init");
 	return args;
 }
 
